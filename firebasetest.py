21afdb2481281a927465241d73a8badcb5f10257
helpmynamewontfi <aydan.jiwani@gmail.com>
2019-09-14 12:54:24 -0400

    created basic database auth/read/write function
    note: they currently exist in a vacuum and must be integrated with frontend (just call them) and azure (gib blockarray)

--- firebasetest.py
+++ firebasetest.py
@@ -1,5 +1,7 @@
 import json
 import pyrebase
+import requests
+
 
 config = {
   "apiKey": "AIzaSyAn2bI9-r1lQrRdao7QQ6GUXu2ZK-f9Hvc",
@@ -8,15 +10,45 @@ config = {
   "storageBucket": "htn-aydan.appspot.com"
   }
 
+
+headers = {
+    'Content-Type': 'application/json',
+}
+
+params = (
+    ('key', 'AIzaSyAn2bI9-r1lQrRdao7QQ6GUXu2ZK-f9Hvc'),
+)
+
+data = '{"returnSecureToken":true}'
+
+
 firebase = pyrebase.initialize_app(config)
 
 db = firebase.database()
 data = {"Transcripts": {1: "Beginning of lecture"} }
 db.child("Classes").child("MATH137").set(data)
 
+def signIn(classID):
+    if(classID == "password"):
+        response = requests.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp', headers=headers, params=params, data=data)
+        usrId = response[idToken])
+        try:
+            firebase_admin.auth.verify_id_token(usrId, app=None, check_revoked=False)
+            return True
+        except:
+            return False
+
+def uploadNewBlock(blockArray):
+    blockID = len(blockArray)-1
+    db.child("Classes").child(Classcode).child("Transcripts").child(blockID).set(blockarray[-1])
+
 def addNote(classCode, blockNumber, message):
-    db.child("Classes").child(classCode).child("Transcripts").child(blockNumber).set(message)
+        db.child("Classes").child(classCode).child("Transcripts").child(blockNumber).set(message)
 def viewAllNotes(classCode, blockNumber):
     return(db.child("Classes").child(classCode).child("Transcripts").child(blockNumber).shallow().get())
 def viewAllBlocks(classCode):
     return(db.child("Classes").child(classCode).child("Transcripts").shallow().get())
+def viewStream(classCode):
+    
+    
+    
